27cc1233b1f0e870ad24ef2b7d55ede3e8015af7
Vic Carrasco <carrasco.vic@gmail.com>
2019-05-13 18:27:26 +0200

    classes for channel handling are done and tested

--- lib/arssene.rb
+++ lib/arssene.rb
@@ -1,5 +1,10 @@
 require "arssene/version"
-
+require "arssene/lib/rss"
+require 'rss'
+require 'open-uri'
+require 'sanitize'
+require 'mechanize'
+require 'byebug'
 module Arssene
   class Error < StandardError; end
   # Your code goes here...
