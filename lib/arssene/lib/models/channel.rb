681a0a47e7a9132b9def0c71ccb0c00dfd219c35
Vic Carrasco <1703579+viccarrasco@users.noreply.github.com>
2020-02-27 15:32:12 +0100

    starting refactoring arssene

---
+++ lib/arssene/lib/models/channel.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Arssene
+  class Channel
+    attr_accessor :title
+    attr_accessor :link
+    attr_accessor :host
+    attr_accessor :feed_type
+    attr_accessor :feed_version
+    attr_accessor :description
+    attr_accessor :publication_date
+    attr_accessor :language
+    attr_accessor :copyright
+    attr_accessor :entries
+    attr_accessor :meta
+    attr_accessor :relevant
+  end
+end
---
+++ lib/arssene/lib/models/entry.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Arssene
+  class Entry
+    attr_accessor :title
+    attr_accessor :link
+    attr_accessor :description
+    attr_accessor :content
+    attr_accessor :publication_date
+    attr_accessor :author
+  end
+end
