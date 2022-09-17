8f6234e25a3e00b5c80233052c8d4f5a94c829b1
Vic Carrasco <1703579+viccarrasco@users.noreply.github.com>
2022-09-17 15:12:00 +0200

    refactor repositories for feed/channel

--- lib/arssene/lib/repositories/channel_repository.rb
+++ lib/arssene/lib/repositories/channel_repository.rb
@@ -5,22 +5,24 @@ module Arssene
     def fetch_as_channel(url)
       begin
         channel = Arssene::Channel.new
-        open(url) do |rss|
-          feed = RSS::Parser.parse(rss)
-          url = URI.parse(feed.channel.link)
-          channel.title = feed.channel.title
-          channel.link  = feed.channel.link
-          channel.host  = url.host
-          channel.feed_type = feed.feed_type
-          channel.feed_version = feed.feed_version
-          channel.description = feed.channel.description
-          channel.publication_date = feed.channel.pubDate
-          channel.language = feed.channel.language ? feed.channel.language.downcase : nil
-          channel.copyright = feed.channel.copyright
-          channel.entries = extract_items feed
-          channel.meta = feed
-          channel.relevant = true
-        end
+
+        response = Faraday.get(url)
+        raise "Unable to reach feed: #{url} | error corde: #{response.status}" unless response.status == 200
+
+        feed = RSS::Parser.parse(response.body)
+        url = URI.parse(feed.channel.link)
+        channel.title = feed.channel.title
+        channel.link  = feed.channel.link
+        channel.host  = url.host
+        channel.feed_type = feed.feed_type
+        channel.feed_version = feed.feed_version
+        channel.description = feed.channel.description
+        channel.publication_date = feed.channel.pubDate
+        channel.language = feed.channel.language ? feed.channel.language.downcase : nil
+        channel.copyright = feed.channel.copyright
+        channel.entries = extract_items feed
+        channel.meta = feed
+        channel.relevant = true
       rescue StandardError => e
         return ({ error: e.to_s })
       end
--- lib/arssene/lib/repositories/feed_repository.rb
+++ lib/arssene/lib/repositories/feed_repository.rb
@@ -6,26 +6,12 @@ module Arssene
       @agent = Mechanize.new
     end
 
-    def ping(uris:)
-      if uris.is_a?(String)
-        retrieve_feed_urls(uris)
-      elsif uris.is_a?(Array)
-        uris.map { |uri| retrieve_feed_urls(uri).first }
-      end
-    end
+    def retrieve_feed_urls(urls)
+      pinged = []
+      embeded_links = embeded_html_source_links(urls)
+      raise 'Non existing feeds' if embeded_links.empty?
 
-    def request(uris:)
-      if uris.is_a?(String)
-      end
-    end
-
-    private
-
-    def retrieve_feed_urls(uri, pinged = [])
-      feed_uris = embeded_html_source_links(uri)
-      raise 'Non existing feeds' if feed_uris.empty?
-
-      pinged = feed_uris.map do |feed|
+      pinged = embeded_links.map do |feed|
         { feed: feed.attr('href').split.join }
       end
     rescue StandardError => e
@@ -34,9 +20,10 @@ module Arssene
       pinged
     end
 
-    def embeded_html_source_links(uri)
-      uri = URI.parse(uri)
-      site = agent.get(uri)
+    private
+
+    def embeded_html_source_links(url)
+      site = agent.get URI.parse(url)
       site.search(".//link[@type='application/rss+xml']")
     end
 
