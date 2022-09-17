5e3f123399839b7c63fe5dfdf79db66ceac540bb
Vic Carrasco <1703579+viccarrasco@users.noreply.github.com>
2022-09-17 15:11:10 +0200

    refactor feed class

--- lib/arssene/lib/interactions/feed.rb
+++ lib/arssene/lib/interactions/feed.rb
@@ -2,12 +2,14 @@
 
 module Arssene
   class Feed
-    def self.ping(urls:)
-      Arssene::Ping.new.request(urls: urls)
+    def self.ping(urls)
+      Arssene::Ping.new
+                   .request(urls)
     end
 
-    def self.request(urls:, **filters)
-      Arssene::Fetch.new.request(urls, **filters)
+    def self.request(urls, **filters)
+      Arssene::Fetch.new
+                    .request(urls, **filters)
     end
   end
 end
